f9e946e33ca4395eab2c4f33c880ad812c27df77
Vadivelan S R <vadivelan.sr@gmail.com>
2016-01-20 15:15:35 +0530

    docs: Added documentation
    Added comments for JokeActivity.java

--- jokeactivity/src/main/java/com/example/vadivelansr/jokeactivity/JokeActivity.java
+++ jokeactivity/src/main/java/com/example/vadivelansr/jokeactivity/JokeActivity.java
@@ -3,7 +3,9 @@ package com.example.vadivelansr.jokeactivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-
+/**
+ *Activity which provides Jokes 
+ */
 public class JokeActivity extends AppCompatActivity {
     public final static String INTENT_JOKE = "INTENT_JOKE";
 
